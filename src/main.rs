863fc8697d191de752671f3ca5c7590a078a97b1
Will Hopkins <willothyh@gmail.com>
2024-02-03 03:38:50 -0800

    feat: more debug terminal setup based on sphaerophoria's stream
    I am developing this egui-based dev environment based on the "Termie"
    streams to help build an escape sequence parser without building a whole
    TUI first, since I can setup an egui app much quicker.

--- src/main.rs
+++ src/main.rs
@@ -6,72 +6,164 @@
 //! from the server to the client which is very limiting, but this will allow for scrollback,
 //! multiple panes, and proper keymappings.
 use std::{
-    ffi::{CStr, CString},
-    io::{BufReader, Read},
-    ops::Deref,
-    os::fd::{FromRawFd, OwnedFd},
-    pin::Pin,
-    process::Stdio,
-    sync::{Arc, RwLock},
-    task::Context,
-};
-use tokio::{
-    fs::File,
-    io::{AsyncRead, AsyncReadExt},
-    sync::Mutex,
+    ffi::CStr,
+    os::fd::{AsRawFd, OwnedFd},
 };
 
 use anyhow::Result;
 use eframe;
-use egui;
-use nix::pty::ForkptyResult;
+use egui::{self, TextStyle, Vec2};
+use nix::{
+    errno::Errno,
+    fcntl::{FcntlArg, OFlag},
+    libc::O_ACCMODE,
+    pty::ForkptyResult,
+};
 
-mod pty;
+#[derive(Debug, Clone)]
+struct CursorPos {
+    x: usize,
+    y: usize,
+}
+
+impl CursorPos {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
 
 pub struct TermGui {
-    buffer: Arc<tokio::sync::RwLock<Vec<u8>>>,
-    file: Arc<Mutex<std::fs::File>>,
+    buffer: Vec<u8>,
+    cursor: CursorPos,
+    char_size: Option<Vec2>,
+    fd: OwnedFd,
+}
+
+fn get_char_size(ctx: &egui::Context) -> Vec2 {
+    let font_id = ctx.style().text_styles[&egui::TextStyle::Monospace].clone();
+    ctx.fonts(|fonts| {
+        let layout = fonts.layout(
+            "A".to_string(),
+            font_id,
+            egui::Color32::default(),
+            f32::INFINITY,
+        );
+
+        Vec2::new(layout.rect.width(), layout.rect.height())
+    })
+}
+
+fn char_to_cursor_offset(char_pos: &CursorPos, char_size: &Vec2, buffer: &[u8]) -> Vec2 {
+    let lines = buffer.split(|b| *b == b'\n').collect::<Vec<_>>();
+
+    let x_off = char_pos.x as f32 * char_size.x;
+    let y_off = (char_pos.y as isize - lines.len() as isize) as f32 * char_size.y;
+    Vec2::new(x_off, y_off)
 }
 
 impl TermGui {
-    fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        cc.egui_ctx.style_mut(|style| {
+            style.override_text_style = Some(TextStyle::Monospace);
+        });
+        let flags = nix::fcntl::fcntl(fd.as_raw_fd(), FcntlArg::F_GETFL).expect("fcntl");
+        let mut flags = OFlag::from_bits(flags & O_ACCMODE).unwrap();
+        // set fd to nonblocking
+        flags.set(OFlag::O_NONBLOCK, true);
+        nix::fcntl::fcntl(fd.as_raw_fd(), FcntlArg::F_SETFL(flags)).expect("fcntl");
         Self {
-            file: Arc::new(Mutex::new(std::fs::File::from(fd))),
-            buffer: Arc::new(tokio::sync::RwLock::new(Vec::new())),
+            fd,
+            cursor: CursorPos::new(0, 0),
+            char_size: None,
+            buffer: Vec::new(),
         }
     }
 
-    fn run(&mut self) {
-        std::thread::spawn({
-            let buffer = self.buffer.clone();
-            let file = self.file.clone();
-            move || {
-                let rt = tokio::runtime::Builder::new_current_thread()
-                    .enable_all()
-                    .build()
-                    .expect("Failed to build tokio runtime");
-                let mut buf = vec![0; 4096];
-                rt.block_on(async move {
-                    loop {
-                        match file.lock().await.read(&mut buf) {
-                            Ok(bytes) => {
-                                buffer.write().await.extend_from_slice(&buf[..bytes]);
-                            }
-                            Err(_) => {
-                                break;
-                            }
-                        }
-                    }
-                });
-            }
-        });
+    fn init(&mut self, ctx: &egui::Context) {
+        self.char_size = Some(get_char_size(ctx));
     }
 }
 
 impl eframe::App for TermGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if let None = self.char_size {
+            self.init(ctx);
+            println!("proportions: {:?}\n", self.char_size);
+        }
+        let mut buf = vec![0u8; 4096];
+        match nix::unistd::read(self.fd.as_raw_fd(), &mut buf) {
+            Ok(n_bytes) => {
+                let bytes = &buf[..n_bytes];
+                // TODO: refactor to push chunks of bytes to the buffer
+                // by tracking the last special character, continuing while
+                // the next character is not a special character, and then
+                // pushing bytes[last_special+1..current] to the buffer.
+                for byte in bytes.iter().copied() {
+                    match byte {
+                        b'\n' => {
+                            self.buffer.push(b'\n');
+                            self.cursor.x = 0;
+                            self.cursor.y += 1;
+                        }
+                        b'\r' => {
+                            self.cursor.x = 0;
+                        }
+                        b'\t' => {
+                            self.cursor.x += 4;
+                        }
+                        _ => {
+                            self.buffer.push(byte);
+                            self.cursor.x += 1;
+                        }
+                    }
+                }
+            }
+            Err(Errno::EAGAIN) => {}
+            Err(e) => {
+                println!("Error reading from fd: {:?}", e);
+                ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+            }
+        }
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(unsafe { std::str::from_utf8_unchecked(&self.buffer.blocking_read()) })
+            ui.input(|state| {
+                for event in state.events.iter() {
+                    let mut bytes = match event {
+                        egui::Event::Key {
+                            key: egui::Key::Enter,
+                            pressed: true,
+                            ..
+                        } => b"\n",
+                        egui::Event::Text(text) => text.as_bytes(),
+                        _ => &[],
+                    };
+                    while bytes.len() > 0 {
+                        match nix::unistd::write(self.fd.as_raw_fd(), &bytes) {
+                            Ok(written) => {
+                                bytes = &bytes[written..];
+                            }
+                            Err(Errno::EAGAIN) => continue,
+                            Err(_) => break,
+                        };
+                    }
+                }
+            });
+
+            let res = ui.label(unsafe { std::str::from_utf8_unchecked(&self.buffer) });
+
+            let bottom = res.rect.bottom();
+            let left = res.rect.left();
+            let painter = ui.painter();
+            let char_size = self.char_size.as_ref().expect("char size to have been set");
+            let cursor_offset = char_to_cursor_offset(&self.cursor, &char_size, &self.buffer);
+
+            painter.rect_filled(
+                egui::Rect::from_min_size(
+                    egui::Pos2::new(left + cursor_offset.x, bottom + cursor_offset.y),
+                    char_size.clone().into(),
+                ),
+                0.0,
+                egui::Color32::GRAY,
+            );
         });
     }
 }
@@ -102,8 +194,7 @@ fn main() -> Result<()> {
         "Debug GUI",
         native_options,
         Box::new(|cc| {
-            let mut app = TermGui::new(cc, fd);
-            app.run();
+            let app = TermGui::new(cc, fd);
             Box::new(app)
         }),
     )
